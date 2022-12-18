d439772dfbdd455855e8183fd5723cbd66fa6e44
Fr0stbyteR <renshihong@hotmail.com>
2022-12-18 22:04:56 +0800

    fix group expand bug in faust-ui

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -948,7 +948,7 @@ define("./service-worker.js",['./workbox-11808712'], function (workbox) { 'use s
     "revision": "ed0bb4ef2ded005f6f0cf7351ea21406"
   }, {
     "url": "faust-ui/index.js",
-    "revision": "0096b2fd9a02ca6f873bb705e40235ad"
+    "revision": "fdb113e19e8d9ba0b08bd6e56ce7fd6c"
   }, {
     "url": "favicon.png",
     "revision": "8c793ef1d04148eb48e9595e54950310"
