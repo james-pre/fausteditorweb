7926f167f7af28225f5f9d58d3b8d9026f2d235b
Fr0stbyteR <renshihong@hotmail.com>
2023-10-21 00:29:53 +0800

    update to faust-ui 1.1.5

--- dist/faust-ui/index.js
+++ dist/faust-ui/index.js
@@ -3263,7 +3263,7 @@ class HGroup extends _AbstractGroup__WEBPACK_IMPORTED_MODULE_0__["default"] {
       }
       item.expand(dX$, dY$);
     });
-    this.layout.width += dX;
+    // this.layout.width += dX;
     return this;
   }
   offset() {
@@ -3710,7 +3710,7 @@ class VGroup extends _AbstractGroup__WEBPACK_IMPORTED_MODULE_0__["default"] {
       }
       item.expand(dX$, dY$);
     });
-    this.layout.height += dY;
+    // this.layout.height += dY;
     return this;
   }
   offset() {
